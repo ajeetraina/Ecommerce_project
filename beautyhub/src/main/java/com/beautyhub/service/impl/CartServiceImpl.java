a2955d878824abfcd528610662d3826163d990b5
Shefali Bisht <shefalibisht00@gmail.com>
2016-08-04 21:18:35 +0530

    Create CartServiceImpl.java

---
+++ beautyhub/src/main/java/com/beautyhub/service/impl/CartServiceImpl.java
@@ -0,0 +1,25 @@
+package com.beautyhub.service.impl;
+
+
+import com.beautyhub.dao.CartDao;
+import com.beautyhub.model.Cart;
+import com.beautyhub.service.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class CartServiceImpl implements CartService{
+
+    @Autowired
+    private CartDao cartDao;
+
+    public Cart getCartById(int cartId){
+        return cartDao.getCartById(cartId);
+    }
+
+    public void update(Cart cart){
+        cartDao.update(cart);
+    }
+
+} // The End of Class;
